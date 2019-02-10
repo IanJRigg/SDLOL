a0abfb2b2be23239aac111e771b136892deb26e3
Ian <ianjrigg@gmail.com>
2019-02-09 17:04:42 -0800

    Add timer test to suite

---
+++ test/test-timer.cpp
@@ -0,0 +1,41 @@
+#include "catch.hpp"
+
+#include "timer.h"
+
+#include <unistd.h>
+
+TEST_CASE("Timer API")
+{
+    SECTION("Timer counts up for a duration")
+    {
+        Timer timer;
+        timer.start();
+
+        usleep(10000UL);
+
+        REQUIRE(timer.current_time() >= 10UL);
+    }
+
+    SECTION("Timer doesn't count up while stopped")
+    {
+        Timer timer;
+
+        usleep(10000UL);
+
+        REQUIRE(timer.current_time() < 10UL);
+    }
+
+    SECTION("Timer doesn't count up when stopping after starting")
+    {
+        Timer timer;
+        timer.start();
+
+        usleep(10000UL);
+
+        timer.stop();
+
+        usleep(10000UL);
+
+        REQUIRE(timer.current_time() < 20UL);
+    }
+}
