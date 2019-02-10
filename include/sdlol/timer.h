1c1cabc68988e747d0d7707ae4dba01367fbe4c8
Ian <ianjrigg@gmail.com>
2019-02-09 17:05:43 -0800

    Remove pausing functions from timer

--- include/sdlol/timer.h
+++ include/sdlol/timer.h
@@ -19,22 +19,14 @@ public:
     void start();
     void stop();
 
-    void pause();
-    void unpause();
-
     uint32_t current_time();
 
     bool is_started();
     bool is_not_started();
 
-    bool is_paused();
-    bool is_not_paused();
-
 private:
     uint32_t m_start_time;
-    uint32_t m_pause_time;
 
-    bool m_is_paused;
     bool m_is_started;
 
 };
--- src/timer.cpp
+++ src/timer.cpp
@@ -4,8 +4,6 @@
 
 Timer::Timer() :
     m_start_time(0UL),
-    m_pause_time(0UL),
-    m_is_paused(false),
     m_is_started(false)
 {
 
@@ -14,55 +12,18 @@ Timer::Timer() :
 void Timer::start()
 {
     m_is_started = true;
-    m_is_paused = false;
-
-    m_start_time = 0UL;
-    m_pause_time = 0UL;
+    m_start_time = SDL_GetTicks();
 }
 
 void Timer::stop()
 {
    m_is_started = false;
-   m_is_paused = false;
-
    m_start_time = 0UL;
-   m_pause_time = 0UL;
-}
-
-void Timer::pause()
-{
-    if(this->is_started() && this->is_not_paused())
-    {
-        m_is_paused = true;
-
-        m_pause_time = SDL_GetTicks() - m_start_time;
-        m_start_time = 0UL;
-    }
-}
-
-void Timer::unpause()
-{
-    if(this->is_started() && this->is_paused())
-    {
-        m_is_paused = false;
-
-        m_start_time = SDL_GetTicks() - m_pause_time;
-        m_pause_time = 0UL;
-    }
 }
 
 uint32_t Timer::current_time()
 {
-    uint32_t time = 0UL;
-
-    if(this->is_started())
-    {
-        time = (this->is_not_paused())
-            ? (SDL_GetTicks() - m_start_time)
-            : m_pause_time;
-    }
-
-    return time;
+    return (this->is_started()) ? (SDL_GetTicks() - m_start_time) : 0UL;
 }
 
 bool Timer::is_started()
@@ -73,14 +34,4 @@ bool Timer::is_started()
 bool Timer::is_not_started()
 {
     return !(this->is_started());
-}
-
-bool Timer::is_paused()
-{
-    return m_is_started && m_is_paused;
-}
-
-bool Timer::is_not_paused()
-{
-    return !(this->is_paused());
 }
